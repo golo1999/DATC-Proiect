60659efe709ae0e80e2351c3c5e63900722a76ed
George-Alexandru Gologan <aggologan1999@gmail.com>
2021-11-21 17:44:54 +0200

    Adding Firebase to the web app

---
+++ web-app/src/Firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+
+const {
+  REACT_APP_API_KEY: API_KEY,
+  REACT_APP_AUTH_DOMAIN: AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID: PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET: STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID: MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID: APP_ID,
+  REACT_APP_REALTIME_DATABASE_URL: DATABASE_URL,
+} = process.env;
+
+const config = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth;
+export const db = firebase.database();
