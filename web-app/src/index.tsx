fb55015ef39fb76f2face729889358f3e26217a7
George-Alexandru Gologan <aggologan1999@gmail.com>
2021-12-25 00:38:35 +0200

    Migrated index (root) & firebase (utility) to TypeScript

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -2,9 +2,9 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import store from "./store/.";
+import store from "./store";
 
-import App from "./App.tsx";
+import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
